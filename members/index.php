b8d892bbe0c73b2f5abc992585f68aaa3aa68c53
Chen Huang <chenhuang444@gmail.com>
2016-10-21 02:42:54 -0500

    changed some

--- members/index.php
+++ members/index.php
@@ -3,41 +3,53 @@ $title = "Executive Board";
 $cssArray = array("./css/members_style.css");
  ?>
  <?php require($_SERVER["DOCUMENT_ROOT"] . "/template/cesa-header.php"); ?>
- <?php 
+ <?php
     function doMembers($fileName){
+        # Department type is index 5 #
+
         echo '<div class="container">';
+        $mydict = [];
         if(($handle = fopen($fileName, 'r')) !== FALSE){
             $num = 0;
             while(($csvArray = fgetcsv($handle)) !== FALSE){
-                doSingleMember($csvArray, $num);
+                $departmentKey = $csvArray[5];
+                if(!array_key_exists($departmentKey, $mydict)){
+                    $mydict[$departmentKey] = array();
+                }
+                $mydict[$departmentKey][] = $csvArray;
+            }
+            foreach ($mydict as $key => $array) {
+                doDepartment($array, $num, $key);
                 $num++;
             }
         }
-        
+
         echo '</div>';
 
     }
 
-    function doSingleMember($csvArray, $num){
+    function doDepartment($csvArray, $num, $departmentName){
         $grey = $num % 2;
-        echo <<<EOD
+        # start of tag for alternating color department block
+        echo '<div class="row member$grey">';
 
-    <div class="row member$grey">
+        # do stuff for each leader
+        echo <<<EOD
             <div class="col-md-4">
                 <a href="#">
                     <img class="img-responsive" src="./img/board_thumbnail/$num.jpg.thumbnail" alt="">
                 </a>
             </div>
             <div class="col-md-8">
-                <h3 class="membername">$csvArray[0] $csvArray[1]</h3>
-                <h4 class="memberattribute">$csvArray[2] $csvArray[3]</h4>
+                <h4 class="membername">$csvArray[0] $csvArray[1]</h3>
+                <h5 class="memberattribute">$csvArray[2] $csvArray[3]</h4>
                 <p class="memberintro">$csvArray[4]</p>
             </div>
-    </div>
-
-
-
 EOD;
+
+
+        # end of tag for department block
+        echo "</div>";
 
     }
 
