5835f14a9df36ec212c30d53e6dd6d2570c23cd0
Chen Huang <chenhuang444@gmail.com>
2016-10-21 03:47:45 -0500

    mostly finished ececutive board page

--- members/index.php
+++ members/index.php
@@ -30,27 +30,49 @@ $cssArray = array("./css/members_style.css");
 
     function doDepartment($csvArray, $num, $departmentName){
         $grey = $num % 2;
+        # class for grid size, change if needed
+        $gridClass = "md";
+
         # start of tag for alternating color department block
-        echo '<div class="row member$grey">';
+        echo '<div class="row member'.$grey.'">';
+        echo '<h3 class="departmentname">'.$departmentName.'</h3>';
 
-        # do stuff for each leader
-        echo <<<EOD
-            <div class="col-md-4">
-                <a href="#">
-                    <img class="img-circle" src="./img/board_thumbnail/$num.jpg.thumbnail.cropped" alt="">
-                </a>
-            </div>
-            <div class="col-md-8">
-                <h4 class="membername">$csvArray[0] $csvArray[1]</h3>
-                <h5 class="memberattribute">$csvArray[2] $csvArray[3]</h4>
-                <p class="memberintro">$csvArray[4]</p>
-            </div>
-EOD;
-
+        $len = count($csvArray);
+        $index = 0;
+        while($len >= 3){
+            echo '<div class="row">';
+                doMember($gridClass, 4, $csvArray[$index], $num);
+                doMember($gridClass, 4, $csvArray[$index+1], $num);
+                doMember($gridClass, 4, $csvArray[$index+2], $num);
+            echo '</div>';
+            $index += 3;
+            $len -= 3;
+        }
+        if($len > 0){
+            echo '<div class="row">';
+            for ($i=0; $i < $len; $i++) {
+                doMember($gridClass, 12/$len, $csvArray[$index + $i], $num);
+            }
+            echo '</div>';
+        }
 
         # end of tag for department block
         echo "</div>";
 
+    }
+
+    function doMember($gridClass, $span, $csvArray, $num){
+        $name = $csvArray[0];
+        echo<<<EOD
+            <div class="col-$gridClass-$span">
+                <a href="#">
+                    <img class="img-circle center-align img-responsive" src="./img/board_thumbnail/$name.jpg.thumbnail.cropped" alt="">
+                </a>
+                <h4 class="membername center-align">$csvArray[0] $csvArray[1]</h3>
+                <h5 class="memberattribute center-align">$csvArray[2] $csvArray[3]</h4>
+                <p class="memberintro center-align">$csvArray[4]</p>
+            </div>
+EOD;
     }
 
 
